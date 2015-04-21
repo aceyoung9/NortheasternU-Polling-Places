5a0e36f9a8413a4ea8313a435f9c895b199524db
Alice Young <aliceyoung.9@gmail.com>
2015-04-21 04:31:34 -0400

    Added form and cover boilerplate from Bootstrap Examples

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: "Polling Places for Northeastern Huskies" });
 });
 
 module.exports = router;
