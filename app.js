188feb0362f80c6c8645916414cc854ede482292
Alice Young <aliceyoung.9@gmail.com>
2015-04-22 21:59:45 -0400

    what are you doing here, boilerplate.

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -34,7 +33,6 @@ fs.readFile('public/javascripts/dorms.json', { encoding : "utf-8" }, function (e
 }); */
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
