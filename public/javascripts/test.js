e1a94e2e8fa17e76df03fe5951f00d85b72bacd8
Alice Young <aliceyoung9@users.noreply.github.com>
2018-10-13 23:02:43 -0400

    Update Northeastern Dorm List. (#3)

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -1,24 +1,27 @@
-var dorms = ["Burstein Hall",
+var dormList = [
+  "Burstein Hall",
+  "1110 Commonwealth Avenue",
   "10 Coventry Street",
   "Davenport Commons A",
   "Davenport Commons B",
   "Douglass Park Leased Properties",
   "East Village",
+  "319 Huntington Avenue",
   "International Village",
   "Kennedy Hall",
   "Kerr Hall",
   "Levine Hall",
   "Light Hall",
+  "144 Hemenway Street",
   "153 Hemenway Street",
-  "319 Huntington Avenue",
-  "Loftman",
+  "Loftman Hall",
   "Melvin Hall",
   "Rubenstein Hall",
   "Smith Hall",
   "Speare Hall",
-  "St. Stephen St. Complex",
   "Stetson East",
   "Stetson West",
+  "St. Stephen St. Complex",
   "West Village A North",
   "West Village A South",
   "West Village B",
@@ -27,33 +30,36 @@ var dorms = ["Burstein Hall",
   "West Village F",
   "West Village G",
   "West Village H",
-  "Willis Hall",
-  "White Hall"
+  "White Hall",
+  "Willis Hall"
 ];
 
 // constructs the suggestion engine
 var dorms = new Bloodhound({
-  datumTokenizer: Bloodhound.tokenizers.obj.whitespace('value'),
+  datumTokenizer: Bloodhound.tokenizers.obj.whitespace("value"),
   queryTokenizer: Bloodhound.tokenizers.whitespace,
-  // `dorms` is an array of state names defined in "The Basics",
-  local: $.map(dorms, function(state) { return { value: state }; })
+  local: $.map(dormList, function(state) {
+    return { value: state };
+  })
 });
 
 // kicks off the loading/processing of `local` and `prefetch`
 dorms.initialize();
 
-$('#prefetch .typeahead').typeahead({
-  hint: true,
-  highlight: true,
-  minLength: 1
-},
-{
-  name: 'dorms',
-  displayKey: 'value',
-  // `ttAdapter` wraps the suggestion engine in an adapter that
-  // is compatible with the typeahead jQuery plugin
-  source: dorms.ttAdapter()
-});
+$("#prefetch .typeahead").typeahead(
+  {
+    hint: true,
+    highlight: true,
+    minLength: 1
+  },
+  {
+    name: "dorms",
+    displayKey: "value",
+    // `ttAdapter` wraps the suggestion engine in an adapter that
+    // is compatible with the typeahead jQuery plugin
+    source: dorms.ttAdapter()
+  }
+);
 
-  // typeahead.js is overly opinionated about their gross-ass stylings.
+// typeahead.js is overly opinionated about their gross-ass stylings.
 $(".tt-hint").addClass("form-control");
